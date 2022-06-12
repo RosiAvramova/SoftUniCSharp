b4cec16861a242b8d39c081989174d682e396ceb
RosiAvramova <rosi.avramova1@gmail.com>
2022-06-12 16:08:00 +0300

    Invalid Number

---
+++ InvalidNumber/InvalidNumber.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _10._InvalidNumber
+{
+    internal class InvalidNumber
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            bool isValidNumber = number >= 100 && number <= 200 || number == 0;
+            if (!isValidNumber)
+            {
+                Console.WriteLine("invalid");
+            }
+
+        }
+    }
+}
