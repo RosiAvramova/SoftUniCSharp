1b982186bc44c57e202313d8f5af81e9483e4648
RosiAvramova <rosi.avramova1@gmail.com>
2022-06-18 13:25:18 +0300

    Task for walking

---
+++ Walking/Walking.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _04Walking
+{
+    internal class Walking
+    {
+        static void Main(string[] args)
+        {
+            int wantedSteps = 10000;
+            int totalSteps = 0;
+            int stepsOverGoal = 0;
+            int stepsToReached = 0;
+
+            while (totalSteps < wantedSteps)
+            {
+                int steps = 0;
+                string input = Console.ReadLine();
+                if (input == "Going home")
+                {
+                    input = Console.ReadLine();
+                    steps = int.Parse(input);
+                    totalSteps += steps;
+                    stepsOverGoal = totalSteps - wantedSteps;
+                    stepsToReached = wantedSteps - totalSteps;
+                    break;
+                }
+
+                steps = int.Parse(input);
+                totalSteps += steps;
+                stepsOverGoal = totalSteps - wantedSteps;
+                stepsToReached = wantedSteps - totalSteps;
+            }
+
+            if (wantedSteps <= totalSteps)
+            {
+                Console.WriteLine($"Goal reached! Good job!");
+                Console.WriteLine($"{stepsOverGoal} steps over the goal!");
+            }
+            else
+            {
+                Console.WriteLine($"{stepsToReached} more steps to reach goal.");
+            }
+        }
+    }
+}
